55e2306155ef47529118ac46d922ca94dfd66503
Rafa Tárrega <rafatarre@gmail.com>
2024-06-10 17:33:36 +0200

    feat: changes

--- app/(routes)/product/[productSlug]/components/info-product.tsx
+++ app/(routes)/product/[productSlug]/components/info-product.tsx
@@ -8,32 +8,41 @@ import { ProductType } from "@/types/product";
 import { Heart } from "lucide-react";
 
 export type InfoProductProps = {
-    product: ProductType
-}
+  product: ProductType;
+};
 
 const InfoProduct = (props: InfoProductProps) => {
-    const { product } = props
-    const { addItem } = useCart()
-    const { addLoveItem } = useLovedProducts()
+  const { product } = props;
+  const { addItem } = useCart();
+  const { addLoveItem } = useLovedProducts();
 
-    return (
-        <div className="px-6">
-            <div className="justify-between mb-3 sm:flex">
-                <h1 className="text-2xl">{product.attributes.productName}</h1>
+  return (
+    <div className="px-6">
+      <div className="justify-between mb-3 sm:flex">
+        <h1 className="text-2xl">{product.attributes.productName}</h1>
 
-                <ProductTasteOrigin origin={product.attributes.origin} taste={product.attributes.taste} />
-            </div>
-            <Separator className="my-4" />
-            <p>{product.attributes.description}</p>
-            <Separator className="my-4" />
-            <p className="my-4 text-2xl">{formatPrice(product.attributes.price)}</p>
-            <div className="flex items-center gap-5">
-                <Button className="w-full " onClick={() => addItem(product)} >Comprar </Button>
-                <Heart width={30} strokeWidth={1} className="transition duration-300 cursor-pointer hover:fill-black"
-                    onClick={() => addLoveItem(product)} />
-            </div>
-        </div>
-    );
-}
+        <ProductTasteOrigin
+          origin={product.attributes.origin}
+          taste={product.attributes.taste}
+        />
+      </div>
+      <Separator className="my-4" />
+      <p>{product.attributes.description}</p>
+      <Separator className="my-4" />
+      <p className="my-4 text-2xl">{formatPrice(product.attributes.price)}</p>
+      <div className="flex items-center gap-5">
+        <Button className="w-full " onClick={() => addItem(product)}>
+          Comprar{" "}
+        </Button>
+        <Heart
+          width={30}
+          strokeWidth={1}
+          className="transition duration-300 cursor-pointer hover:fill-black"
+          onClick={() => addLoveItem(product)}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default InfoProduct;
